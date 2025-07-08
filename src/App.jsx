732806c19cfd32e6ef11c862886a7a93b77e910a
Ayan Mahapatra <ayanmahapatra4@gmail.com>
2025-07-09 00:50:22 +0530

    Updating employee UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Login from './components/Auth/Login'
+import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <>
       {/* <Login></Login> */}
-      {/* <EmployeeDashboard></EmployeeDashboard> */}
-      <AdminDashboard></AdminDashboard>
+      <EmployeeDashboard></EmployeeDashboard>
+      {/* <AdminDashboard></AdminDashboard> */}
     </>
   )
 }
