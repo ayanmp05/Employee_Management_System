c5dbb7ec371b77125d650a2abd810419c19ddce2
Ayan Mahapatra <ayanmahapatra4@gmail.com>
2025-07-12 23:48:54 +0530

    setting auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './Utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 
-// 1:45:03 uptill
+
 
 const App = () => {
   // User state handling
   const [user, setUser] = useState(null)
+  // Using AuthContext to retrieve data
+  const authData = useContext(AuthContext)
 
+  // useEffect(() => {
+  //   if (authData) {
+  //     const loggedInUser = localStorage.getItem("loggedInUser")
+  //     if (loggedInUser) {
+  //       setUser(loggedInUser.role)
+  //     }
+  //   }
+  // }, [authData])
+  
   // handleLogin setup and passing argument in email and password
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
-    } else if (email == 'user@me.com' && password == '123') {
+      // Setting user role in admin
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
+    } else if (authData && authData.employees.find((e) => e.email == email && e.password == password)) {
       setUser('employee')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
     } else {
       alert("Invalid Credentials")
     }
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { createContext } from 'react'
+import { getLocalStorage } from '../Utils/localStorage'
+//Creating context
+export const AuthContext = createContext()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  // Calling useState for data
+  const [userData, setUserData] = useState(null)
+  // Passing the data
+  useEffect(() => {
+    const { employees, admin } = getLocalStorage()
+    setUserData({ employees, admin })
+  }, [])
+
+
   return (
     <div>
-      {children}
+      {/* Wrapping with AuthContext */}
+      <AuthContext.Provider value={userData}>
+        {children}
+      </AuthContext.Provider>
     </div>
   )
 }
